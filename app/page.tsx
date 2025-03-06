ca528d2495bbc708c8dbb2bf93c9a1453d4f22d2
yunshin <2younsin@gmail.com>
2025-03-06 13:13:31 +0900

    feat: (none) 내부패키지, package.json > dependency 에 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Button, Toggle } from '@lit-ui';
-
 const Init = () => {
   return (
     <div>
       <h1>사자 == Tiger</h1>
-      <Button>감사합니다.</Button>
-      <Toggle></Toggle>
     </div>
   );
 };
