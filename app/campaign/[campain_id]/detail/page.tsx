225d60673004434026cebbceaaa2b371acc0bc12
yunshin <2younsin@gmail.com>
2025-03-06 13:25:33 +0900

    feat: (none) 페이지 초기 구성

---
+++ app/campaign/[campain_id]/detail/page.tsx
@@ -0,0 +1,5 @@
+const CampaignDetail = () => {
+  return <h1>CampaignDetail</h1>;
+};
+
+export default CampaignDetail;
---
+++ app/campaign/[campain_id]/manage/page.tsx
@@ -0,0 +1,5 @@
+const CampaignManage = () => {
+  return <h1>CampaignManage</h1>;
+};
+
+export default CampaignManage;
